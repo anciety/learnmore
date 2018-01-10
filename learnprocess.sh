c51cab6979618433f3abeac2df98131b75c2f12f
Anciety <28222393+anciety@users.noreply.github.com>
2018-01-10 01:06:23 +0100

    Update learnprocess.sh

--- learnprocess.sh
+++ learnprocess.sh
@@ -12,6 +12,7 @@ echo -e "\e[31m########## ########## ###     ### ###    ### ###    ####      ###
 echo -e "\e[31m						Version $VER"
 printf "\nHello, $USER. Today we're going to check some stuff based on your skillset. Are you ready? (y/n) / (!helpmelearn): "
 read opt
+
 if [[ $opt == "y" ]]; then
 	echo "Okay great! Starting with a few questions!"
 	echo -n "What's your age? "
@@ -30,7 +31,7 @@ if [[ $opt == "!helpmelearn" ]]; then
 	[2]  Here's a site where you can get a lot of E-books from: allitebook.com
 	[3]  Being in a community is great to learn, get help and help others is great.. Except if you're in the InstaGram Wannabe DD0S H4x0R scr1pt K1dd13s community..
 	[3+] Well then i'm here for you.. -> https://netsecfocus.slack.com	https://www.reddit.com/r/netsec/	https://www.reddit.com/r/blackhat/
-	[4] There are also places to practise       -->	https://hackthebox.eu	https://vulnhub.com	https://www.cybrary.it/ <--
+	[4] There are also places to practise --> https://hackthebox.eu		https://vulnhub.com		https://www.cybrary.it/ <--
 	[4+] Please don't cheat in the invitation process of HTB if you do, you're a cunt :)\n"
 	exit 1
 fi	
@@ -51,6 +52,7 @@ fi
 		if [[ $opt1 == "y" && $age -gt 17 ]]; then
 			echo -n "Oh okay so you're $age years old, do you have a job? (y/n)"
 			read job
+
 		if [[ $job == "y" ]]; then
 			echo -n "Okay cool, what do you do for a job? "
 			read whatjob
@@ -67,3 +69,5 @@ fi
 	fi
 fi
 fi
+
+# Created By Anciety, https://instagram.com/Anciety	https://twitter.com/postmodules
